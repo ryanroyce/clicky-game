4d8168ff48ca9986d8d3e58ace8a8216b344dec3
dissonance <ryanminor01u@yahoo.com>
2019-03-08 15:59:40 -0600

    simlified card/updated JSON

--- client/src/components/FriendCard/index.js
+++ client/src/components/FriendCard/index.js
@@ -5,23 +5,13 @@ function FriendCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.image} onClick={() => props.selectFriend(props.id)} className="remove"/>
+        <img alt={props.name} src={props.image} onClick={() => props.selectFriend(props.id)} className="remove" />
       </div>
       <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
+        {props.name}
       </div>
     </div>
   );
-}
+};
 
 export default FriendCard;
