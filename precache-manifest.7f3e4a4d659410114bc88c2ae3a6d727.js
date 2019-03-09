c1339efd3d221dbaf47c8360fd612fbb1790f9c3
dissonance <ryanminor01u@yahoo.com>
2019-03-09 13:14:22 -0600

    Updates

---
+++ precache-manifest.7f3e4a4d659410114bc88c2ae3a6d727.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f620db0c0a5e8a91ca5b",
+    "url": "/clicky-game/static/css/main.e3284a45.chunk.css"
+  },
+  {
+    "revision": "f620db0c0a5e8a91ca5b",
+    "url": "/clicky-game/static/js/main.f620db0c.chunk.js"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/clicky-game/static/js/2.ff661de6.chunk.js"
+  },
+  {
+    "revision": "b4874bde90304a680c5eebe4156a4e2e",
+    "url": "/clicky-game/index.html"
+  }
+];
